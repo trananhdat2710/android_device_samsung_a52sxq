9a32cffbdf7f04646dc5ff294cbe3f6f99f72848
AD <103117507+trananhdat2710@users.noreply.github.com>
2025-04-25 10:17:12 +0700

    Update aosp_a52sxq.mk

--- aosp_a52sxq.mk
+++ aosp_a52sxq.mk
@@ -25,7 +25,7 @@ TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
@@ -38,7 +38,7 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_HAVE_SEC_NFC := true
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_a52sxq
+PRODUCT_NAME := lineage_a52sxq
 PRODUCT_DEVICE := a52sxq
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A528B
